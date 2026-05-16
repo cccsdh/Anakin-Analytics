806d795b0bb646a6954a49bda2456e267874ebde
S. Doug Hunt <ccc_sdh@comcast.net>
2026-05-16 07:36:01 -0400

    Delete VaderSharp directory
    Removing folder - being replaced with new name

--- VaderSharp/NuGet-Pack.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set version=%1
-
-dotnet restore .\VaderSharp
-dotnet pack .\VaderSharp\VaderSharp.csproj --output ..\nupkgs --configuration Release /p:PackageVersion=%version% --include-symbols --include-source
